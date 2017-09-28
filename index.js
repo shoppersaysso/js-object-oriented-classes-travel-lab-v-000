c99f7d98cf09e65d38e0ed2412cc284647b57ca5
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 02:58:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Driver {
   constructor(name, startDate) {
     this.name = name;
@@ -16,6 +18,11 @@ class Route {
     this.endingLocation = endingLocation;
   }
   blocksTravelled(){
+
+
+    avenueToInteger(avenue){
+    return eastWest.indexOf(avenue)
+  }
     
   }
 }
