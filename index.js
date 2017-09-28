020c89aa7609067ecb1e3e1950a215bf9fc09ad3
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 02:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ class Driver {
   yearsExperienceFromBeginningOf(year){
     let endDate = new Date(year, 1, 1)
     let total = (endDate - this.startDate)/(365*24*60*60*1000)
-    return parseInt(totalYears);
+    return parseInt(total);
   }
 }
