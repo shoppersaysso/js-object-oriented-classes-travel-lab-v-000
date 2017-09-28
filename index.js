5e3aedd80c24bf8c67a5c7d1fa748cbd44472da3
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 02:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+class Driver {
+  constructor(name, joinDate)
+  
+}
 
+
+
+
+
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+//  
+//   sayHello() {
+//     console.log(`Hello, my name is ${this.name}`);
+//   }
+// }
