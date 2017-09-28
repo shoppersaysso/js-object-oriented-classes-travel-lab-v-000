5a8bb132bffaadd69f9a581e108309d4e6537550
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 03:11:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,20 @@ class Route {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
-  blocksTravelled(){
-  }
-
+  
   avenueToInteger(avenue){
     return eastWest.indexOf(avenue)
   }
-
+  blocksTravelled(){
+    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+  }
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
 }
